c7a6bdfa5c95fe429ea4799d6b5d716447521167
mosiahvb <mosiahvaldezbates@gmail.com>
2023-06-15 21:10:25 -0600

    finished progrct

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        PromptGenerator promptGenerator= new PromptGenerator();
+        Journal journal = new Journal();
+        int menuUserInput = 0;
+
+        List<string>  menu =new List<String>
+        {
+            "",
+            "MENU",
+            "please select one of the following choices",
+            "1 Write",
+            "2 Display",
+            "3 Load",
+            "4 Save",
+            "5 Quit",
+            "what would you like to do?"
+        };
+        Console.WriteLine("welcome to the journal program");
+
+        while(menuUserInput !=5)
+        {
+            foreach (string menuItem in menu)
+            {
+                Console.WriteLine(menuItem);
+            }
+            menuUserInput= int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            switch (menuUserInput)
+            {
+                case 1:
+                    string prompt = promptGenerator.GetRandomPrompt();
+                    Console.WriteLine();
+                    Console.WriteLine(prompt);
+                    Console.Write("> ");
+                    string response = Console.ReadLine();
+                    string date = DateTime.Now.Date.ToString("MMM dd, yyyy");
+
+                    Entry entry = new Entry(date, prompt, response);
+                    journal.AddEntry(entry);
+                    break;
+                case 2:
+                    journal.DisplayAll();
+                    break;
+                case 3:
+                    Console.WriteLine("what is the filename?");
+                    string fileNameLoad = Console.ReadLine();
+                    journal.LoadFromFile(fileNameLoad);
+                    break;
+                case 4:
+                    Console.WriteLine("what is the filename?");
+                    string fileNameSave = Console.ReadLine();
+                    journal.SaveToFile(fileNameSave);
+                    break;
+                case 5:
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    Console.WriteLine();
+                    break;
+          
+          }
+        }
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,29 @@
+using System;
+//using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    List<string> prompts;  
+
+    public PromptGenerator() 
+    {
+        prompts = new List<string> // create a new list to be held inside of list-prompts 
+        {
+            "who made you smile today, and how did they do it? ",
+            "what did you learn today that you didnt expect to learn?",
+            "how did you make a positive impact on someone's life today?",
+            "what did you see when you looked at your family today?",
+            "(take a moment) what are you missing in your life, what do you need to do to achieve it?"
+        };
+    }
+
+    public string GetRandomPrompt()
+    {
+        string randomPrompt = "";
+        int randomIndex= new Random().Next(prompts.Count);                                             
+        randomPrompt = prompts[randomIndex];
+
+        return randomPrompt;
+
+    }
+}
